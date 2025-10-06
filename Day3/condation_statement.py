11a35bfb2f14a9d4397625f00872c16febda2715
AjayNavghare <111764506+AjayNavghare@users.noreply.github.com>
2025-10-06 07:44:38 +0000

    learn condition statment

---
+++ Day3/condation_statement.py
@@ -0,0 +1,5 @@
+age = 20
+if age>18:
+    print(age," age ready to vote")
+else:
+    print("not ready for vote")
---
+++ Day3/posi_negi.py
@@ -0,0 +1,6 @@
+i = -20
+if i>0:
+    print(f'{i} is positive')
+
+else:
+    print(f'{i} is Negitive')
