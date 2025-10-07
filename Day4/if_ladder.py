57a31caa48cb28a9323baa899ee7d5c80888f974
AjayNavghare <111764506+AjayNavghare@users.noreply.github.com>
2025-10-07 04:51:43 +0000

    learn for loop ,while loop,break statement and continue

---
+++ Day4/if_ladder.py
@@ -0,0 +1,14 @@
+# price = int(input("Enter a Price: "))
+# # price = 220
+
+# if price > 100:
+#     print("you qualify for a Discount ")
+
+#     if price >200:
+#         print("Discount is 20% ")
+#     else:
+#         print("Discount is 10% ")
+# else:
+#     print("No Discount ")
+
+
---
+++ Day4/while.py
@@ -0,0 +1,32 @@
+#While loop
+
+i = 0
+while i < 5:
+    print(i)
+    i +=1
+
+
+print("----------------------------------------------------------------------------------------------------")
+# Break  --   Used to exit the loop immediately
+
+# for i in range(10):
+#     if i == 5:
+#        break
+#     print(i)
+
+#Continue  --  Skips the current iteration and moves to the next.
+
+# for i in range(10):
+#     if i == 5:
+#         continue
+#     print(i)
+
+
+#Foe loop --  Used when you know how many times to repeat.
+
+for i in range(5):
+    print(i)
+
+
+
+
