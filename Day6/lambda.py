d617511627b25d520b66582da9257bb14914f106
AjayNavghare <111764506+AjayNavghare@users.noreply.github.com>
2025-10-09 05:43:07 +0000

    learn lambda function and exception handing is going on

--- Day6/lambda.py
+++ Day6/lambda.py
@@ -1,1 +1,15 @@
-num 
+add = lambda x, y:x+y
+result = add(10,20)
+print(result)
+
+print("------------for Square----------------")
+
+square = lambda x:x*x
+result = square(5)
+print(result)
+
+
+print("-----------check for Even Numbers-----------------")
+is_even = lambda x: x%2==0
+result = is_even(4)
+print(result)
---
+++ Day6/lambda_fun.py
@@ -0,0 +1,8 @@
+is_even = lambda x: "Even" if x%2 ==0 else "Odd"
+
+print(is_even(4))
+print(is_even(5))
+
+print("-------------filter Even Numbers from a List---------------------")
+nums = [1,2,3,4,5,6]
+evens list(lambda x:x%2==0),nums
